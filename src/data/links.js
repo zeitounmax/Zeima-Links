4d8f45ef4e95a38fa6c31c330e4822a7068e9aa3
Zeima <mthiry1@gmail.com>
2025-05-19 18:53:54 +0200

    new Link

--- src/data/links.js
+++ src/data/links.js
@@ -1,8 +1,9 @@
 export const links = [
     {label: "Mon site web", url: "https://zeitouncode.fr"},
     {label: "Mon blog", url: "https://zeima08.com"},
+    {label: "Peertube", url: "https://video.ploud.jp/a/zeimaa/"},
     {label: "Bluesky", url: "https://bsky.app/profile/lezeima.fr"},
     {label: "Mastodon", url: "https://shelter.moe/@Zeitounmax"},
     {label: "Matrix", url: "@zeima:matrix.org"},
-    {label: "Peetube", url: "zeimaa@video.ploud.jp"}
+    
 ];
