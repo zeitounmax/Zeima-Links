85488b4878088bd6bf3e22fbbcd2d6bda106bf9f
Zeima <mthiry1@gmail.com>
2025-05-19 18:51:27 +0200

    new Link

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#Zeima-Links
+# Zeima-Links
 
 
 It's just an page with my Links into social network and Website. 
--- src/data/links.js
+++ src/data/links.js
@@ -3,5 +3,6 @@ export const links = [
     {label: "Mon blog", url: "https://zeima08.com"},
     {label: "Bluesky", url: "https://bsky.app/profile/lezeima.fr"},
     {label: "Mastodon", url: "https://shelter.moe/@Zeitounmax"},
-    {label: "Matrix", url: "@zeima:matrix.org"}
+    {label: "Matrix", url: "@zeima:matrix.org"},
+    {label: "Peetube", url: "zeimaa@video.ploud.jp"}
 ];
