3588503c47f23e8fd163aa6ae53a80e15dd4934a
Zeima <mthiry1@gmail.com>
2025-05-20 19:07:53 +0200

    Pixelfed link

--- src/data/links.js
+++ src/data/links.js
@@ -1,9 +1,9 @@
 export const links = [
     {label: "Mon site web", url: "https://zeitouncode.fr"},
     {label: "Mon blog", url: "https://zeima08.com"},
-    {label: "Peertube", url: "https://video.ploud.jp/a/zeimaa/"},
     {label: "Bluesky", url: "https://bsky.app/profile/lezeima.fr"},
     {label: "Mastodon", url: "https://shelter.moe/@Zeitounmax"},
+    {label: "Pixelfed", url: "https://pixelfed.fr/Zeima"},
+    {label: "Peertube", url: "https://video.ploud.jp/a/zeimaa/"},
     {label: "Matrix", url: "@zeima:matrix.org"},
-    
 ];
